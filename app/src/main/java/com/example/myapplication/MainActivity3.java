e99a4b297291e1e086e503616b49b15e85d89b59
Chaehyun Lee <cogusdl1113@gmail.com>
2021-06-01 21:38:14 +0900

    지역 검색 페이지 추가

--- app/src/main/java/com/example/myapplication/MainActivity3.java
+++ app/src/main/java/com/example/myapplication/MainActivity3.java
@@ -2,7 +2,11 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity3 extends AppCompatActivity {
 
@@ -10,5 +14,18 @@ public class MainActivity3 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Button okButton = (Button) findViewById(R.id.ok);
+        okButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                EditText city = (EditText)findViewById(R.id.city);
+                String strCity = city.getText().toString();
+                Intent intent = new Intent(MainActivity3.this,MainActivity4.class);
+                intent.putExtra("city",strCity);
+                startActivity(intent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/myapplication/MainActivity4.java
+++ app/src/main/java/com/example/myapplication/MainActivity4.java
@@ -2,13 +2,25 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity4 extends AppCompatActivity {
+
+    private TextView tCity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main4);
+
+        tCity = findViewById(R.id.tCity);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("city");
+        //String strUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + str + "&appid=867d2553fb743f162631454d6ebde974&lang=kr";
+
+        tCity.setText(str);
     }
 }
